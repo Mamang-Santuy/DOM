df23b6e88ba75cfcb9eebf05cfb36dafa05c9194
unknown <sayidrofi2951@gmail.com>
2025-10-08 07:52:29 +0700

    Modif dikit bagian submit form

---
+++ EventInput&Change/app.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('#form');
+const input = document.querySelector('input');
+const list = document.querySelector('#notes');
+
+input.addEventListener('change', (e)=>{
+    console.log('nilai diubah');
+});
+
+input.addEventListener('input', (e)=>{
+    document.querySelector('h1').innerText = input.value;
+    console.log('nilai diinput');
+});
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    const noteValue = input.value;
+    const newList = document.createElement('li');
+    newList.innerText = noteValue;
+    list.append(newList);
+    input.value = '';
+});
+
